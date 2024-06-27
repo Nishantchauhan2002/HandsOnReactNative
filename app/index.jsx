adbfc2d3f41132710b75a3054a472bd70eabaafa
Nishant Chauhan <nishant031102@gmail.com>
2024-06-27 13:44:26 +0530

    Text added

--- app/index.jsx
+++ app/index.jsx
@@ -26,6 +26,26 @@ const RootLayout = () => {
             className="max-w-[380px] w-full h-[298px]"
             resizeMode="contain"
           />
+
+<View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+          
           </View>
 
       </ScrollView>
