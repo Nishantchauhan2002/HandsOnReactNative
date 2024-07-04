1c004e931b10ae60e907cf990968ce0713666ad4
Nishant Chauhan <nishant031102@gmail.com>
2024-07-04 12:22:03 +0530

    Fields added in the sign in page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,19 @@
 import { View, Text, ScrollView, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        {/* <View className="w-full justify-center h-full px-4 my-6"> */}
+        <View className="">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -19,7 +24,19 @@ const SignIn = () => {
           {" "}
           Log in to myApp
         </Text>
-        <FormField />
+        <FormField
+          title="Email"
+          value={form.email}
+          handleChangeText={(e) => setForm({ ...form, email: e })}
+          otherStyles="mt-7"
+          keyboardType="email-address"
+        />
+        <FormField
+          title="Password"
+          value={form.password}
+          handleChangeText={(p) => setForm({ ...form, password: p })}
+          otherStyles="mt-7"
+        />
       </ScrollView>
     </SafeAreaView>
   );
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
+import { useState } from "react";
 import React from "react";
 
-const FormField = () => {
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setshowPassword] = useState(false);
   return (
-    <View className="space-y-2">
-      <Text className="text-base text-gray-100 font-pmedium">FormField</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View
+        className="border-2 
+      border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center"
+      >
+        <TextInput
+          className="flex-1 text-white font-psemibold"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry
+        ></TextInput>
+      </View>
     </View>
   );
 };
