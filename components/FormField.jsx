4b247711a5321578b6e0600ed7e25ec9c672a989
Nishant Chauhan <nishant031102@gmail.com>
2024-07-04 13:11:26 +0530

    Textfield issue resolved

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -16,15 +16,15 @@ const FormField = ({
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
       <View
         className="border-2 
-      border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center"
+      border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary flex flex-row items-center"
       >
         <TextInput
-          className="flex-1 text-white font-psemibold"
+          className="flex-1 text-white font-semibold text-base"
           value={value}
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
           onChangeText={handleChangeText}
-          secureTextEntry
+          secureTextEntry={title === "Password" && !showPassword}
         ></TextInput>
       </View>
     </View>
